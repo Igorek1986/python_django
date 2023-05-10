750acf6039878061df8fcb3528301932106f4750
Igor Ponomarev <9129910001@mail.ru>
2023-05-10 20:17:13 +0500

    fix task 1, task 2 completed

--- M_05_RequestsAndMiddlewares/mysite/requestandmiddlewaresapp/views.py
+++ M_05_RequestsAndMiddlewares/mysite/requestandmiddlewaresapp/views.py
@@ -6,10 +6,13 @@ from django.shortcuts import render
 def handle_file_upload_limit(request: HttpRequest) -> HttpResponse:
     if request.method == "POST" and request.FILES.get("myfile"):
         myfile = request.FILES["myfile"]
-        fs = FileSystemStorage()
-        fs.save(myfile.name, myfile)
         if request.FILES.get("myfile").size > 1048576:
             return render(request, "requestandmiddlewaresapp/error_upload.html")
+        else:
+            fs = FileSystemStorage()
+            fs.save(myfile.name, myfile)
     return render(request, "requestandmiddlewaresapp/file-upload.html")
-    # else:
-    # return render(request, "requestandmiddlewaresapp/error_upload.html")
+
+
+def error_time_access(request: HttpRequest) -> HttpResponse:
+    return render(request, "requestandmiddlewaresapp/error-time.html")
