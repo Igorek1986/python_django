adc1ca034ff42770543ceeea03548f0eff2650ab
Igor Ponomarev <9129910001@mail.ru>
2023-11-16 23:44:09 +0500

    added translation for the pages 'Product list' and 'Product detail'

--- i18n_l10n/mysite/mysite/urls.py
+++ i18n_l10n/mysite/mysite/urls.py
@@ -17,18 +17,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shop/', include('shopapp.urls')),
-    path('myauth/', include('myauth.urls')),
+    # path('admin/', admin.site.urls),
+    # path('shop/', include('shopapp.urls')),
+    path("myauth/", include("myauth.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns.extend(
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    )
+urlpatterns += i18n_patterns(
+    path("admin/", admin.site.urls),
+    path("shop/", include("shopapp.urls")),
+)
 
-    urlpatterns.extend(
-        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    )
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
+    urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
