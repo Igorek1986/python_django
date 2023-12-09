caeb7a76a203c88f9beb2ef2733cc9fab7186643
Igor Ponomarev <9129910001@mail.ru>
2023-12-09 15:56:07 +0500

    added logging

--- logging/mysite/mysite/urls.py
+++ logging/mysite/mysite/urls.py
@@ -19,16 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shop/', include('shopapp.urls')),
-    path('myauth/', include('myauth.urls')),
+    path("admin/", admin.site.urls),
+    path("shop/", include("shopapp.urls")),
+    path("myauth/", include("myauth.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns.extend(
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    )
-
-    urlpatterns.extend(
-        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    )
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
