4605179b7cc3e089283d77dd702658fd669f8094
Igor Ponomarev <9129910001@mail.ru>
2023-12-12 21:18:42 +0500

    added method get_absolute, added sitemap, added Feed for Products

--- import_export/mysite/mysite/urls.py
+++ import_export/mysite/mysite/urls.py
@@ -17,18 +17,26 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import sitemaps
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shop/', include('shopapp.urls')),
-    path('myauth/', include('myauth.urls')),
+    path("admin/", admin.site.urls),
+    path("shop/", include("shopapp.urls")),
+    path("myauth/", include("myauth.urls")),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemaps",
+    ),
 ]
 
 if settings.DEBUG:
     urlpatterns.extend(
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     )
 
     urlpatterns.extend(
-        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
     )
--- import_export/mysite/shopapp/views.py
+++ import_export/mysite/shopapp/views.py
@@ -1,14 +1,29 @@
 from timeit import default_timer
 
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, JsonResponse
-from django.shortcuts import render, reverse
-from django.urls import reverse_lazy
+from django.http import (
+    HttpResponse,
+    HttpRequest,
+    HttpResponseRedirect,
+    JsonResponse,
+)
+from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.syndication.views import Feed
 
 from .forms import ProductForm
 from .models import Product, Order, ProductImage
@@ -41,15 +56,15 @@ class ProductViewSet(ModelViewSet):
 class ShopIndexView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         products = [
-            ('Laptop', 1999),
-            ('Desktop', 2999),
-            ('Smartphone', 999),
+            ("Laptop", 1999),
+            ("Desktop", 2999),
+            ("Smartphone", 999),
         ]
         context = {
             "time_running": default_timer(),
             "products": products,
         }
-        return render(request, 'shopapp/shop-index.html', context=context)
+        return render(request, "shopapp/shop-index.html", context=context)
 
 
 class ProductDetailsView(DetailView):
@@ -105,26 +120,17 @@ class ProductDeleteView(DeleteView):
 
 
 class OrdersListView(LoginRequiredMixin, ListView):
-    queryset = (
-        Order.objects
-        .select_related("user")
-        .prefetch_related("products")
-        .all()
-    )
+    queryset = Order.objects.select_related("user").prefetch_related("products").all()
 
 
 class OrderDetailView(PermissionRequiredMixin, DetailView):
     permission_required = "shopapp.view_order"
-    queryset = (
-        Order.objects
-        .select_related("user")
-        .prefetch_related("products")
-    )
+    queryset = Order.objects.select_related("user").prefetch_related("products")
 
 
 class ProductsDataExportView(View):
     def get(self, request: HttpRequest) -> JsonResponse:
-        products = Product.objects.order_by('pk').all()
+        products = Product.objects.order_by("pk").all()
         products_data = [
             {
                 "pk": product.pk,
@@ -135,3 +141,18 @@ class ProductsDataExportView(View):
             for product in products
         ]
         return JsonResponse({"products": products_data})
+
+
+class LatestProductsFeed(Feed):
+    title = "Product (latest)"
+    description = "Update, changes, addition products"
+    link = reverse_lazy("shopapp:products_list")
+
+    def items(self):
+        return Product.objects.filter(archived=False).order_by("-created_at")[:5]
+
+    def item_title(self, item: Product) -> str:
+        return item.name
+
+    def item_description(self, item: Product) -> str:
+        return item.description[:150]
